d7b0f57a733673228e53a8438a5a277d1a20c945
Kevin Raoofi <nullindev@gmail.com>
2014-06-30 19:37:18 -0400

    Moved old tests to separate test directory

---
+++ src/test/java/org/compbox/jaywalker/PathCopyTest.java
@@ -0,0 +1,34 @@
+package org.compbox.jaywalker;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+//import java.util.List;
+
+import org.compbox.jaywalker.ShresthaVistor;
+
+public class PathCopyTest {
+	public static void main(String[] args) {
+		Path ashwin = Paths.get("C:\\Users\\kevin\\Dropbox\\peopledump\\shrestha");
+		Path jay = Paths.get("C:\\Users\\kevin\\jaytest");
+		ShresthaVistor ashvisitor = new ShresthaVistor();
+		ShresthaVistor jayvisitor = new ShresthaVistor();
+		try {
+			
+			Files.walkFileTree(ashwin, ashvisitor);
+			Files.walkFileTree(jay, jayvisitor);
+			
+			//List<Path> foo = ashvisitor.getImageList();
+			
+			
+			
+		} catch (IOException e) {
+		}
+		
+		
+		
+	}
+}
